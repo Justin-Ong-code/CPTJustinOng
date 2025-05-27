a1cca558b4327bcf8fee9d94bc7afe275765f1cc
Justin-Ong-code <justin.ong26@ycdsbk12.ca>
2025-05-27 10:53:52 -0400

    Create poker.java

---
+++ poker.java
@@ -0,0 +1,12 @@
+import arc.*;
+
+public class poker{
+	public static void main(String[] args){
+		Console con = new Console();
+		int intDeck[][][];
+		int intRand;
+		
+		intDeck = new int[52][4][];
+		intRand = (int)(Math.random() * 100 + 1);
+	}
+}
