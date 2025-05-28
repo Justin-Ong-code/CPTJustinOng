b7efd467c6689c76ac9ad5343636089b261c6ddf
Justin-Ong-code <justin.ong26@ycdsbk12.ca>
2025-05-28 10:57:46 -0400

    May 28
    - 2d array consists of 52 rows and 3 columns
    - intDeck was changed to intCard

--- poker.java
+++ poker.java
@@ -3,10 +3,13 @@ import arc.*;
 public class poker{
 	public static void main(String[] args){
 		Console con = new Console();
-		int intDeck[][][];
+		int intCard[][];
 		int intRand;
+		int intCount;
 		
-		intDeck = new int[52][4][];
-		intRand = (int)(Math.random() * 100 + 1);
+		intCard = new int[52][3];
+		for(intCount = 0; intCount < 52; intCount++){
+			intRand = (int)(Math.random() * 100 + 1);
+		}
 	}
 }
